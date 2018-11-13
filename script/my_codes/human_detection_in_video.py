c6b49c19acc400cf372c42af66e15165933276cf
ThivakaranThana <thivakaranthana@gmail.com>
2018-11-13 13:29:06 +0000

    updating tensorflow_humandetection

--- script/my_codes/human_detection_in_video.py
+++ script/my_codes/human_detection_in_video.py
@@ -9,9 +9,12 @@ import time
 import socket
 import struct
 import pickle
+from PIL import Image
+from scipy.spatial import distance as dist
+from collections import OrderedDict
 
-from script.my_codes.centroidtracker import CentroidTracker
-from script.my_codes.nonmax_suppression import nms
+#from script.my_codes.centroidtracker import CentroidTracker
+#from script.my_codes.nonmax_suppression import nms
 
 
 class DetectorAPI:
@@ -64,12 +67,227 @@ class DetectorAPI:
     def close(self):
         self.sess.close()
         self.default_graph.close()
+        
+def nms(boxes, overlap_threshold=0.2, mode='union'):
+    """Non-maximum suppression.
+
+    Arguments:
+        boxes: a float numpy array of shape [n, 5],
+            where each row is (xmin, ymin, xmax, ymax, score).
+        overlap_threshold: a float number.
+        mode: 'union' or 'min'.
+
+    Returns:
+        list with indices of the selected boxes
+    """
+
+    # if there are no boxes, return the empty list
+    if len(boxes) == 0:
+        return []
+
+    # list of picked indices
+    pick = []
+
+    # grab the coordinates of the bounding boxes
+    x1, y1, x2, y2, score = [boxes[:, i] for i in range(5)]
+
+    area = (x2 - x1 + 1.0)*(y2 - y1 + 1.0)
+    ids = np.argsort(score)  # in increasing order
+
+    while len(ids) > 0:
+
+        # grab index of the largest value
+        last = len(ids) - 1
+        i = ids[last]
+        pick.append(i)
+
+        # compute intersections
+        # of the box with the largest score
+        # with the rest of boxes
+
+        # left top corner of intersection boxes
+        ix1 = np.maximum(x1[i], x1[ids[:last]])
+        iy1 = np.maximum(y1[i], y1[ids[:last]])
+
+        # right bottom corner of intersection boxes
+        ix2 = np.minimum(x2[i], x2[ids[:last]])
+        iy2 = np.minimum(y2[i], y2[ids[:last]])
+
+        # width and height of intersection boxes
+        w = np.maximum(0.0, ix2 - ix1 + 1.0)
+        h = np.maximum(0.0, iy2 - iy1 + 1.0)
+
+        # intersections' areas
+        inter = w * h
+        if mode == 'min':
+            overlap = inter/np.minimum(area[i], area[ids[:last]])
+        elif mode == 'union':
+            # intersection over union (IoU)
+            overlap = inter/(area[i] + area[ids[:last]] - inter)
+
+        # delete all boxes where overlap is too big
+        ids = np.delete(
+            ids,
+            np.concatenate([[last], np.where(overlap > overlap_threshold)[0]])
+        )
+
+    return pick
+
+class CentroidTracker():
+	def __init__(self, maxDisappeared=50):
+		# initialize the next unique object ID along with two ordered
+		# dictionaries used to keep track of mapping a given object
+		# ID to its centroid and number of consecutive frames it has
+		# been marked as "disappeared", respectively
+		self.nextObjectID = 0
+		self.objects = OrderedDict()
+		self.disappeared = OrderedDict()
+
+		# store the number of maximum consecutive frames a given
+		# object is allowed to be marked as "disappeared" until we
+		# need to deregister the object from tracking
+		self.maxDisappeared = maxDisappeared
+
+	def register(self, centroid):
+		# when registering an object we use the next available object
+		# ID to store the centroid
+		self.objects[self.nextObjectID] = centroid
+		self.disappeared[self.nextObjectID] = 0
+		self.nextObjectID += 1
+
+	def deregister(self, objectID):
+		# to deregister an object ID we delete the object ID from
+		# both of our respective dictionaries
+		del self.objects[objectID]
+		del self.disappeared[objectID]
+
+	def update(self, rects):
+		# check to see if the list of input bounding box rectangles
+		# is empty
+		if len(rects) == 0:
+			# loop over any existing tracked objects and mark them
+			# as disappeared
+			for objectID in self.disappeared.keys():
+				self.disappeared[objectID] += 1
+
+				# if we have reached a maximum number of consecutive
+				# frames where a given object has been marked as
+				# missing, deregister it
+				if self.disappeared[objectID] > self.maxDisappeared:
+					self.deregister(objectID)
+
+			# return early as there are no centroids or tracking info
+			# to update
+			return self.objects
+
+		# initialize an array of input centroids for the current frame
+		inputCentroids = np.zeros((len(rects), 2), dtype="int")
+
+		# loop over the bounding box rectangles
+		for (i, (startX, startY, endX, endY)) in enumerate(rects):
+			# use the bounding box coordinates to derive the centroid
+			cX = int((startX + endX) / 2.0)
+			cY = int((startY + endY) / 2.0)
+			inputCentroids[i] = (cX, cY)
+
+		# if we are currently not tracking any objects take the input
+		# centroids and register each of them
+		if len(self.objects) == 0:
+			for i in range(0, len(inputCentroids)):
+				self.register(inputCentroids[i])
+
+		# otherwise, are are currently tracking objects so we need to
+		# try to match the input centroids to existing object
+		# centroids
+		else:
+			# grab the set of object IDs and corresponding centroids
+			objectIDs = list(self.objects.keys())
+			objectCentroids = list(self.objects.values())
+
+			# compute the distance between each pair of object
+			# centroids and input centroids, respectively -- our
+			# goal will be to match an input centroid to an existing
+			# object centroid
+			D = dist.cdist(np.array(objectCentroids), inputCentroids)
+
+			# in order to perform this matching we must (1) find the
+			# smallest value in each row and then (2) sort the row
+			# indexes based on their minimum values so that the row
+			# with the smallest value as at the *front* of the index
+			# list
+			rows = D.min(axis=1).argsort()
+
+			# next, we perform a similar process on the columns by
+			# finding the smallest value in each column and then
+			# sorting using the previously computed row index list
+			cols = D.argmin(axis=1)[rows]
+
+			# in order to determine if we need to update, register,
+			# or deregister an object we need to keep track of which
+			# of the rows and column indexes we have already examined
+			usedRows = set()
+			usedCols = set()
+
+			# loop over the combination of the (row, column) index
+			# tuples
+			for (row, col) in zip(rows, cols):
+				# if we have already examined either the row or
+				# column value before, ignore it
+				# val
+				if row in usedRows or col in usedCols:
+					continue
+
+				# otherwise, grab the object ID for the current row,
+				# set its new centroid, and reset the disappeared
+				# counter
+				objectID = objectIDs[row]
+				self.objects[objectID] = inputCentroids[col]
+				self.disappeared[objectID] = 0
+
+				# indicate that we have examined each of the row and
+				# column indexes, respectively
+				usedRows.add(row)
+				usedCols.add(col)
+
+			# compute both the row and column index we have NOT yet
+			# examined
+			unusedRows = set(range(0, D.shape[0])).difference(usedRows)
+			unusedCols = set(range(0, D.shape[1])).difference(usedCols)
+
+			# in the event that the number of object centroids is
+			# equal or greater than the number of input centroids
+			# we need to check and see if some of these objects have
+			# potentially disappeared
+			if D.shape[0] >= D.shape[1]:
+				# loop over the unused row indexes
+				for row in unusedRows:
+					# grab the object ID for the corresponding row
+					# index and increment the disappeared counter
+					objectID = objectIDs[row]
+					self.disappeared[objectID] += 1
+
+					# check to see if the number of consecutive
+					# frames the object has been marked "disappeared"
+					# for warrants deregistering the object
+					if self.disappeared[objectID] > self.maxDisappeared:
+						self.deregister(objectID)
+
+			# otherwise, if the number of input centroids is greater
+			# than the number of existing object centroids we need to
+			# register each new input centroid as a trackable object
+			else:
+				for col in unusedCols:
+					self.register(inputCentroids[col])
+
+		# return the set of trackable objects
+		return self.objects
 
 if __name__ == "__main__":
     model_path = '../../faster_rcnn_inception_v2_coco_2018_01_28/frozen_inference_graph.pb'
     odapi = DetectorAPI(path_to_ckpt=model_path)
     threshold = 0.7
-    cap = cv2.VideoCapture('/home/niruhan/Dataset/custom_video/vedha.mov')
+    #cap = cv2.VideoCapture('/home/niruhan/Dataset/custom_video/vedha.mov')
+    cap = cv2.VideoCapture('/path/to/input/video')
     encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
 
     # connect to socket
--- script/my_codes/tensorflow-human-detection.py
+++ script/my_codes/tensorflow-human-detection.py
@@ -6,8 +6,10 @@ import numpy as np
 import tensorflow as tf
 import cv2
 import time
+from PIL import Image
 
-from script.my_codes.nonmax_suppression import nms
+
+#from script.my_codes.nonmax_suppression import nms
 
 
 class DetectorAPI:
@@ -60,6 +62,71 @@ class DetectorAPI:
     def close(self):
         self.sess.close()
         self.default_graph.close()
+        
+def nms(boxes, overlap_threshold=0.2, mode='union'):
+    """Non-maximum suppression.
+
+    Arguments:
+        boxes: a float numpy array of shape [n, 5],
+            where each row is (xmin, ymin, xmax, ymax, score).
+        overlap_threshold: a float number.
+        mode: 'union' or 'min'.
+
+    Returns:
+        list with indices of the selected boxes
+    """
+
+    # if there are no boxes, return the empty list
+    if len(boxes) == 0:
+        return []
+
+    # list of picked indices
+    pick = []
+
+    # grab the coordinates of the bounding boxes
+    x1, y1, x2, y2, score = [boxes[:, i] for i in range(5)]
+
+    area = (x2 - x1 + 1.0)*(y2 - y1 + 1.0)
+    ids = np.argsort(score)  # in increasing order
+
+    while len(ids) > 0:
+
+        # grab index of the largest value
+        last = len(ids) - 1
+        i = ids[last]
+        pick.append(i)
+
+        # compute intersections
+        # of the box with the largest score
+        # with the rest of boxes
+
+        # left top corner of intersection boxes
+        ix1 = np.maximum(x1[i], x1[ids[:last]])
+        iy1 = np.maximum(y1[i], y1[ids[:last]])
+
+        # right bottom corner of intersection boxes
+        ix2 = np.minimum(x2[i], x2[ids[:last]])
+        iy2 = np.minimum(y2[i], y2[ids[:last]])
+
+        # width and height of intersection boxes
+        w = np.maximum(0.0, ix2 - ix1 + 1.0)
+        h = np.maximum(0.0, iy2 - iy1 + 1.0)
+
+        # intersections' areas
+        inter = w * h
+        if mode == 'min':
+            overlap = inter/np.minimum(area[i], area[ids[:last]])
+        elif mode == 'union':
+            # intersection over union (IoU)
+            overlap = inter/(area[i] + area[ids[:last]] - inter)
+
+        # delete all boxes where overlap is too big
+        ids = np.delete(
+            ids,
+            np.concatenate([[last], np.where(overlap > overlap_threshold)[0]])
+        )
+
+    return pick
 
 if __name__ == "__main__":
     model_path = '../../faster_rcnn_inception_v2_coco_2018_01_28/frozen_inference_graph.pb'
@@ -67,34 +134,38 @@ if __name__ == "__main__":
     threshold = 0.7
     # cap = cv2.VideoCapture('/path/to/input/video')
 
-    img = cv2.imread('my_images/sutha.jpg')
+    img = cv2.imread('my_images/manhattan.jpg')
 
     # img = cv2.resize(cap, (1280, 720))
 
     boxes, scores, classes, num = odapi.processFrame(img)
 
     nms_input = np.empty((len(boxes),5))
-
-    # nms_input[:, :-1] = boxes
-    # nms_input[:, :0] = boxes[1]
-    # nms_input[:, :1] = boxes[0]
-    # nms_input[:, :2] = boxes[3]
-    # nms_input[:, :3] = boxes[2]
-    # nms_input[:, 4] = scores
+    nms_input[:, 0] = [i[1] for i in boxes]
+    nms_input[:, 1] = [i[0] for i in boxes]
+    nms_input[:, 2] = [i[3] for i in boxes]
+    nms_input[:, 3] = [i[2] for i in boxes]
+    nms_input[:, 4] = scores
 
     picks_from_nms = nms(nms_input)
 
     # Visualization of the results of a detection.
 
-    for i in range(len(boxes)):
+    #for i in range(len(boxes)):
         # Class 1 represents human
-        if classes[i] == 1 and scores[i] > threshold:
-            box = boxes[i]
-            cv2.circle(img, (box[1], box[0]), 5, (0, 255, 0), -1)
-            cv2.circle(img, (box[3], box[2]), 5, (255, 0, 0), -1)
-            # cv2.rectangle(img, (box[1], box[0]), (box[3], box[2]), (255, 0, 0), 2)
+     #   if classes[i] == 1 and scores[i] > threshold:
+      #      box = boxes[i]
+            #cv2.circle(img, (box[1], box[0]), 5, (0, 255, 0), -1)
+            #cv2.circle(img, (box[3], box[2]), 5, (255, 0, 0), -1)
+       #    cv2.rectangle(img, (box[1], box[0]), (box[3], box[2]), (255, 0, 0), 2)
+    for i in picks_from_nms:
+        if classes[i]==1 and scores[i] > threshold:
+            box=boxes[i]
+            cv2.rectangle(img, (box[1], box[0]), (box[3], box[2]), (255, 0, 0), 2)
+            
 
-    cv2.imshow("preview", img)
+    #cv2.imshow("preview", img)
+    cv2.imwrite("boundingbox.jpg", img)
     key = cv2.waitKey(0)
 
     # while True:
